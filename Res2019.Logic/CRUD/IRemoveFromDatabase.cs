8aa093b327fad046131d0086d602436fcda4bcdf
Paweł <pawelchorub@gmail.com>
2020-01-16 18:12:54 +0100

    + DeleteDateFromDatabase_NEW method DeleteAppointmentFromDatabase_NEW method

--- Res2019.Logic/CRUD/IRemoveFromDatabase.cs
+++ Res2019.Logic/CRUD/IRemoveFromDatabase.cs
@@ -3,5 +3,9 @@
     public interface IRemoveFromDatabase
     {
         void RemoveAppointmentFromDatabase(string dateOfApp, string timeOfApp);
+        void DeleteDateFromDatabase_NEW(string id);
+
+        void DeleteAppointmentFromDatabase_NEW(string id);
+
     }
 }
--- Res2019.Logic/CRUD/RemoveFromDatabase.cs
+++ Res2019.Logic/CRUD/RemoveFromDatabase.cs
@@ -15,10 +15,42 @@ namespace Res2019
         private static IMsSqlDatabaseSettings connectionString = DatabaseManager.CreateMsSqlDatabaseSettings();
 
         private static SqlConnection sqlConnection = new SqlConnection(connectionString.MsSqlConnectionStringBuild());
+        private static SqlConnection sqlConnection_New = new SqlConnection(connectionString.MsSqlConnectionStringBuild_New());
         private static SqlCommand sqlCommand;
         private string sqlQuery = "";
         private readonly static string table = connectionString.UseReservationTableName();
 
+        public void DeleteDateFromDatabase_NEW(string id)
+        {
+            if (!string.IsNullOrWhiteSpace(id))
+            {
+                sqlConnection_New.Open();
+                sqlQuery = string.Format("DELETE FROM date WHERE date_id = '{0}'", id);
+                sqlCommand = new SqlCommand(sqlQuery, sqlConnection_New);
+                sqlCommand.ExecuteNonQuery();
+                sqlConnection_New.Close();
+            }
+            else
+            {
+                MessageBox.Show("Musisz podać godzinę wizyty, którą chcesz usunąć!");
+            }
+        }
+
+        public void DeleteAppointmentFromDatabase_NEW(string id)
+        {
+            if (!string.IsNullOrWhiteSpace(id))
+            {
+                sqlConnection_New.Open();
+                sqlQuery = string.Format("DELETE FROM appointment WHERE appointment_id = '{0}'", id);
+                sqlCommand = new SqlCommand(sqlQuery, sqlConnection_New);
+                sqlCommand.ExecuteNonQuery();
+                sqlConnection_New.Close();
+            }
+            else
+            {
+                MessageBox.Show("Musisz podać godzinę wizyty, którą chcesz usunąć!");
+            }
+        }
 
         public void RemoveAppointmentFromDatabase(string dateOfAppointment, string timeOfAppointment)
         {
